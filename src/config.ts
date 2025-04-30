6c8e249060e4e01d8607f17582d845f108487725
Nuhman Pk <62880706+nuhmanpk@users.noreply.github.com>
2025-04-30 11:39:38 +0530

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,3 @@
 export const config = {
-    BOT_TOKEN: process.env.BOT_TOKEN || 'YOUR_BOT_TOKEN',
-    SUPERADMIN_ID: process.env.SUPERADMIN_ID || 'YOUR_ADMIN_ID',
-    MONGO_URI: process.env.MONGO_URI || 'YOUR_MONGO_URI'
+    BOT_TOKEN: process.env.BOT_TOKEN || 'YOUR_BOT_TOKEN'
 };
